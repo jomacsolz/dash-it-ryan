8fb41572efa2a32917906dec9e9d5b9a8f93ca2c
jomacsolz <jomacsolz@gmail.com>
2024-11-19 00:01:52 +0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-// #include <raylib.h>
+#include "raylib.h"
 
 int main(){
-    
-    
+    InitWindow(1280, 720, "Run It Ryan");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Run It Ryan", 10, 10, 20, DARKGRAY);
+        EndDrawing();
+    }
+
     return 0;
 }
