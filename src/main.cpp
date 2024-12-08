2d4333eb6eb2b8df06f69065e478584126f6e18a
jomacsolz <jomacsolz@gmail.com>
2024-12-08 20:08:06 +0800

    fixed texture loading and added menus

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <raylib.h>
+#include <vector>
 #include "ryan.hpp"
 #include "bg.hpp"
 #include "obstacle.hpp"
 
-#define NUM_FRAMES 3
+#define NUM_FRAMES 2
 
 const int screenWidth = 1280;
 const int screenHeight = 720;
@@ -34,23 +35,31 @@ int main(){
     const float scoreInterval = 0.1f;
 
     // button
-    Texture2D button = LoadTexture("assets/playButton.png");
-    float frameHeight = (float)button.height/NUM_FRAMES;
-    Rectangle sourceRec = {0, 0, (float)button.width, frameHeight};
-    Rectangle btnBounds = {screenWidth/2.0f - button.width/2.0f, screenHeight/2.0f - button.height/NUM_FRAMES/2.0f, (float)button.width, frameHeight};
-    int btnState = 0;
-    bool btnAction = false;
+    Texture2D playButton = LoadTexture("assets/menu/play.png");
+    Texture2D exitButton = LoadTexture("assets/menu/exit.png");
+    float frameHeight = (float)playButton.height/NUM_FRAMES;
+    Rectangle sourceRecPlay = {0, 0, (float)playButton.width, frameHeight};
+    Rectangle btnBoundsPlay = {screenWidth/2.0f - playButton.width/2.0f, screenHeight/2.0f - playButton.height/NUM_FRAMES/2.0f + 150, (float)playButton.width, frameHeight};
+    Rectangle sourceRecExit = {0, 0, (float)exitButton.width, frameHeight};
+    Rectangle btnBoundsExit = {screenWidth/2.0f - exitButton.width/2.0f, screenHeight/2.0f - exitButton.height/NUM_FRAMES/2.0f + 250, (float)exitButton.width, frameHeight};
+    int btnStatePlay = 0;
+    bool btnActionPlay = false;
+    int btnStateExit = 0;
+    bool btnActionExit = false;
     Vector2 mousePoint = {0.0f, 0.0f};
 
     // Title screen textures
     Texture2D logo = LoadTexture("assets/menu/logo.png");
     Texture2D menu = LoadTexture("assets/menu/flat.png");
+    Texture2D end = LoadTexture("assets/menu/end.png");
+    Texture2D howToPlay0 = LoadTexture("assets/menu/how0.png");
+    Texture2D howToPlay1 = LoadTexture("assets/menu/how1.png");
 
     float scale = 0.5f;
     float posX = (screenWidth - logo.width * scale) / 2;
-    float posY = (screenHeight - logo.height * scale) / 2 - 200;
+    float posY = (screenHeight - logo.height * scale) / 2 - 100;
 
-    // SFX
+    // Audio
     Music gameBGM = LoadMusicStream("assets/audio/ingame.wav");
     Music menuBGM = LoadMusicStream("assets/audio/menu.wav");
     Sound deathSFX = LoadSound("assets/audio/death.wav");
@@ -59,7 +68,8 @@ int main(){
 
     BG bg;
     Player ryan;
-    ObstacleSpawn obby;
+    ObstacleSpawn bugs;
+    Texture2D bugTex = LoadTexture("assets/obstacle.png");
 
     PlayMusicStream(menuBGM);
 
@@ -78,42 +88,81 @@ int main(){
             case TITLE:
             {
                 if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_TAB)){
-                    currentScreen = GAMEPLAY;
-                    StopMusicStream(menuBGM);
-                    PlayMusicStream(gameBGM);
+                    currentScreen = INSTRUCTION1;
                 }
                 mousePoint = GetMousePosition();
-                btnAction = false;
+                btnActionPlay = false;
 
-                if(CheckCollisionPointRec(mousePoint, btnBounds)){
-                    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btnState = 2;
-                    else btnState = 0;
+                if(CheckCollisionPointRec(mousePoint, btnBoundsPlay)){
+                    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btnStatePlay = 0;
+                    else btnStatePlay = 1;
 
-                    if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnAction = true;
-                }else btnState = 0;
+                    if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnActionPlay = true;
+                }else btnStatePlay = 0;
 
-                if(btnAction){
+                if(CheckCollisionPointRec(mousePoint, btnBoundsExit)){
+                    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btnStateExit = 0;
+                    else btnStateExit = 1;
+
+                    if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnActionExit = true;
+                }else btnStateExit = 0;
+
+                if(btnActionPlay){
                     // PlaySound(fxButton); // button sfx
+                    currentScreen = INSTRUCTION1;
+                }
+                if(btnActionExit){
+                    CloseAudioDevice();
+                    CloseWindow();
+                    return 0;
+                }
+
+                sourceRecPlay.y = btnStatePlay*frameHeight;
+                sourceRecExit.y = btnStateExit*frameHeight;
+            } break;
+            case INSTRUCTION1:
+            {
+                if(IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_ENTER)){
+                    currentScreen = INSTRUCTION2;
+                }
+            } break;
+            case INSTRUCTION2:
+            {
+                if(IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_ENTER)){
                     currentScreen = GAMEPLAY;
                     StopMusicStream(menuBGM);
                     PlayMusicStream(gameBGM);
                 }
-
-                sourceRec.y = btnState*frameHeight;
             } break;
             case GAMEPLAY:
             {
-                // if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_TAB))
-                //     currentScreen = ENDING;
+                // Score counter
+                scoreTimer += GetFrameTime();
+                if(scoreTimer >= scoreInterval){
+                    score++;
+                    scoreTimer = 0.0f;
+
+                    if(score % 50 == 0){
+                        bg.SpeedUp(1.0f);
+                        bugs.SpeedUp(0.1f);
+                    }
+                }
             } break;
             case ENDING:
             {
-                if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_TAB)){
+                if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_TAB)){ // return to title screen
                     currentScreen = TITLE;
                     score = 0; // reset score
                     bg.resetSpeed(); // reset bg speed
-                    obby.resetSpeed(); // reset obstacle speed
-                    obby = ObstacleSpawn(); // re-initialize obstacle
+                    bugs.resetSpeed(); // reset obstacle speed
+                    bugs = ObstacleSpawn(); // reset obstacles
+                } else if(IsKeyPressed(KEY_R)){ // restart game
+                    currentScreen = GAMEPLAY;
+                    score = 0; // reset score
+                    bg.resetSpeed(); // reset bg speed
+                    bugs.resetSpeed(); // reset obstacle speed
+                    bugs = ObstacleSpawn(); // reset obstacles
+                    PlayMusicStream(gameBGM);
                 }
             } break;
             default: break;
@@ -121,6 +170,7 @@ int main(){
 
         bg.Update();
         ryan.Update();
+        // bugs.Update(bugTex);
 
         BeginDrawing();
 
@@ -141,39 +191,38 @@ int main(){
                     DrawTextureEx(menu, (Vector2){0,0}, 0.0f, scaleImg(menu), WHITE); // bg image
                     DrawTextureEx(logo, (Vector2){posX,posY}, 0.0f, scale, WHITE); // logo image
 
-                    DrawTextureRec(button, sourceRec, (Vector2){btnBounds.x, btnBounds.y}, WHITE);
+                    DrawTextureRec(playButton, sourceRecPlay, (Vector2){btnBoundsPlay.x, btnBoundsPlay.y}, WHITE);
+                    DrawTextureRec(exitButton, sourceRecExit, (Vector2){btnBoundsExit.x, btnBoundsExit.y}, WHITE);
+                } break;
+                case INSTRUCTION1:
+                {
+                    DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+                    DrawTextureEx(menu, (Vector2){0,0}, 0.0f, scaleImg(menu), WHITE); // bg image
+                    DrawTextureEx(howToPlay0, (Vector2){0,0}, 0.0f, scaleImg(howToPlay0), WHITE);
+                } break;
+                case INSTRUCTION2:
+                {
+                    DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+                    DrawTextureEx(menu, (Vector2){0,0}, 0.0f, scaleImg(menu), WHITE); // bg image
+                    DrawTextureEx(howToPlay1, (Vector2){0,0}, 0.0f, scaleImg(howToPlay0), WHITE);
                 } break;
                 case GAMEPLAY:
                 {
-                    // Score counter
-                    scoreTimer += GetFrameTime();
-                    if(scoreTimer >= scoreInterval){
-                        score++;
-                        scoreTimer = 0.0f;
-
-                        if(score % 50 == 0){
-                            bg.SpeedUp(1.0f);
-                            obby.SpeedUp(0.1f);
-                        }
-                    }
-
                     // Draw Background
                     ClearBackground(RAYWHITE);
                     bg.Draw();
-                    
-
-                    // Obstacle Draw
-                    obby.Update();
-
 
                     // Player Draw
                     ryan.Draw();
-                    // ryan.DrawHitbox(false);
+
+                    // Obstacle Draw
+                    bugs.Update(bugTex);
+                    bugs.Draw();
 
                     // score display
                     DrawText(TextFormat("Score: %i", score), screenWidth-150, 10, 20, WHITE);
-
-                    if(CheckCollision(ryan, obby.getObstacles())){
+                    
+                    if(CheckCollision(ryan, bugs.getObstacles())){
                         currentScreen = ENDING;
                         PlaySound(deathSFX);
                         StopMusicStream(gameBGM);
@@ -182,12 +231,14 @@ int main(){
                 case ENDING:
                 {
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
-                    textWidth = MeasureText("Game Over", 40);
-                    DrawText("Game Over", (screenWidth - textWidth) / 2, (screenHeight - 40) / 2, 40, RED);
-                    textWidth = MeasureText(TextFormat("Score: %i", score), 20);
-                    DrawText(TextFormat("Score: %i", score), (screenWidth - textWidth) / 2, (screenHeight - 40) / 2 + 40, 20, WHITE);
+                    // textWidth = MeasureText("Game Over", 40);
+                    // DrawText("Game Over", (screenWidth - textWidth) / 2, (screenHeight - 40) / 2, 40, RED);
+                    DrawTextureEx(menu, (Vector2){0,0}, 0.0f, scaleImg(menu), WHITE); // bg image
+                    DrawTexture(end, 0, 0, WHITE);
+                    textWidth = MeasureText(TextFormat("Score: %i", score), 40);
+                    DrawText(TextFormat("Score: %i", score), (screenWidth - textWidth) / 2, (screenHeight - 40) / 2 + 40, 40, WHITE);
                     textWidth = MeasureText("Press Enter or Tab to return to title screen", 20);
-                    DrawText("Press Enter or Tab to return to title screen", (screenWidth - textWidth) / 2, (screenHeight - 20) / 2 + 60, 20, GRAY);
+                    DrawText("Press Enter or Tab to return to title screen", (screenWidth - textWidth) / 2, (screenHeight - 20) / 2 + 80, 20, GRAY);
                 } break;
                 default: break;
             }
@@ -195,9 +246,10 @@ int main(){
         EndDrawing();
     }
 
-    UnloadTexture(button);
+    UnloadTexture(playButton);
     UnloadTexture(logo);
     UnloadTexture(menu);
+    UnloadTexture(bugTex);
     UnloadSound(deathSFX);
     UnloadMusicStream(gameBGM);
     UnloadMusicStream(menuBGM);
